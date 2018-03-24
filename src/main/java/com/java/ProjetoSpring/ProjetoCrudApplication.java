5a9f87e433c5d8700422ae574c738db95040c721
Eneylton <eneylton@hotmail.com>
2018-03-24 11:18:53 -0300

    Projeto Criado

---
+++ src/main/java/com/java/ProjetoSpring/ProjetoCrudApplication.java
@@ -0,0 +1,12 @@
+package com.java.ProjetoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCrudApplication.class, args);
+	}
+}
